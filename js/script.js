7eaa06c4c84fe1c368129cf2c12e5d8b3e5a7da6
Victor Purice <victor.purice@ulbsibiu.ro>
2024-02-19 01:29:23 +0200

    started slideshow feature

--- js/script.js
+++ js/script.js
@@ -5,12 +5,36 @@ import { validateForm } from './validation.js';
 
 //validate form
 const form = document.getElementById('contact-form');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+if(form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    if (validateForm(form)) {
-        form.submit();
+        if (validateForm(form)) {
+            form.submit();
+        }
+    });
+}
+
+// slideshow functionality
+export function changeContent(option) {
+    const benefitsCard = document.getElementById('benefits-card');
+    const paragraph = benefitsCard.querySelector('p');
+    const icon = benefitsCard.querySelector('.benefits-icon');
+
+    switch (option) {
+        case 'quality':
+            paragraph.textContent = 'There are more than 100,000 workers registered here at Phlexi, so you would be able to find the most qualitative one.';
+            icon.src = '/images/quality.svg';
+            break;
+        case 'security':
+            paragraph.textContent = 'Your security is our top priority. We employ the latest encryption techniques to keep your information safe.';
+            icon.src = '/images/security.svg';
+            break;
+        case 'care':
+            paragraph.textContent = 'We deeply care about our customers. Our dedicated support team is available 24/7 to assist you.';
+            icon.src = '/images/care.svg';
+            break;
+        default:
+            break;
     }
-});
-
-
+}
