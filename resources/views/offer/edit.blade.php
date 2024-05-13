ee1d4fb3fe9fbcd0b148a9364c68b4877c1f8ca1
Victor Purice <victor.purice@ulbsibiu.ro>
2024-05-13 18:27:27 +0300

    added back link components

--- resources/views/offer/edit.blade.php
+++ resources/views/offer/edit.blade.php
@@ -3,17 +3,8 @@
         Create
     </x-slot>
     <div class="h-screen relative flex justify-center items-center ">
-        <a href="/dashboard" class="absolute top-2 left-2">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" width="40px" height="40px" viewBox="0 0 32 32" version="1.1">
-                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-                    <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-414.000000, -985.000000)" fill="#2563eb">
-                        <path d="M436,1002 L425.414,1002 L429.535,1006.12 C429.926,1006.51 429.926,1007.15 429.535,1007.54 C429.145,1007.93 428.512,1007.93 428.121,1007.54 L422.465,1001.879 C422.225,1001.639 422.15,1001.311 422.205,1001 C422.15,1000.689 422.225,1000.361 422.465,1000.121 L428.121,994.465 C428.512,994.074 429.145,994.074 429.535,994.465 C429.926,994.855 429.926,995.488 429.535,995.879 L425.414,1000 L436,1000 C436.553,1000 437,1000.448 437,1001 C437,1001.553 436.553,1002 436,1002 L436,1002 Z M442,985 L418,985 C415.791,985 414,986.791 414,989 L414,1013 C414,1015.21 415.791,1017 418,1017 L442,1017 C444.209,1017 446,1015.21 446,1013 L446,989 C446,986.791 444.209,985 442,985 L442,985 Z" id="arrow-left-square" sketch:type="MSShapeGroup">
+        <x-back-link></x-back-link>
 
-                        </path>
-                    </g>
-                </g>
-            </svg>
-        </a>
         <div class="mx-auto max-w-2xl container bg-slate-200 dark:bg-slate-900 rounded-lg shadow-lg overflow-hidden">
             <div class="p-6">
                 <h2 class="text-lg font-semibold text-gray-800 dark:text-slate-200 mb-4">Woud you like to accept this offer ?</h2>
